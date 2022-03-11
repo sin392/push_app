3b38e48713c76def418c8f802acb05357ba56605
sin392 <sin392@gmail.com>
2022-03-11 22:39:14 +0900

    small fix

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QuoteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\QuoteController::class, 'index']);
+Route::get('/', [QuoteController::class, 'index']);
