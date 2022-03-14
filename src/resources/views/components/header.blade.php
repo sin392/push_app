3cd7505b009ef66c214eba03c4c44924d57fc9fd
sin392 <sin392@gmail.com>
2022-03-15 08:50:32 +0900

    add a setting button

--- src/resources/views/components/header.blade.php
+++ src/resources/views/components/header.blade.php
@@ -1,8 +1,13 @@
 <header class="header">
     <a href="/" style="text-decoration:none; color: inherit; display: inline-block;">Push App For Intern</a>
     <div>
-        <a href="/" style="text-decoration:none; color: inherit; display: inline-block;">Top</a>
-        <a href="/form" style="text-decoration:none; color: inherit; display: inline-block;">Form</a>
-        <a href="#!" style="text-decoration:none; color: inherit; display: inline-block;">View</a>
+        <div class='nav-items'>
+            <a href="/" style="text-decoration:none; color: inherit; display: inline-block;">Top</a>
+            <a href="/form" style="text-decoration:none; color: inherit; display: inline-block;">Form</a>
+            <a href="#!" style="text-decoration:none; color: inherit; display: inline-block;">View</a>
+        </div>
+        <div class="icon-wrapper">
+            <span class="material-icons">settings</span>
+        </div>
     </div>
 </header>
