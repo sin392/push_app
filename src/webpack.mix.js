56e3e8efa048420c1fe9b261679e87fb0a220f69
sin392 <sin392@gmail.com>
2022-03-15 11:30:07 +0900

    update mix

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require("laravel-mix");
+// require("mix-env-file");
 
 /*
  |--------------------------------------------------------------------------
@@ -6,12 +7,34 @@ const mix = require("laravel-mix");
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
+ | for your Laravel applications. By default, we are compiling the CSS
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix
-    // .js('resources/js/app.js', 'public/js')
-    // .sass("resources/sass/app.scss", "public/css")
-    .sourceMaps();
+mix.js("resources/js/index.js", "public/js").js(
+    "resources/js/form.js",
+    "public/js"
+);
+// not work
+// .browserSync({
+//     // container_name
+//     proxy: {
+//         target: "http://localhost:8080",
+//     },
+//     files: ["resources/views/**/*.blade.php", "public/**/*"],
+//     open: false,
+//     reloadOnRestart: true,
+// });
+
+// .js("resources/js/app.js", "public/js")
+// .postCss("resources/css/app.css", "public/css", [
+//     //
+// ]);
+
+// mix.env(process.env.ENV_FILE);
+
+// キャッシュによりCSSが反映されないのを避けるため
+if (mix.inProduction()) {
+    mix.version();
+}
